93acbf36d150e91ab362bb7a42f5c7f1fef55537
ianmcknnn <ianmcnnn@gmail.com>
2020-06-16 12:45:09 -0400

    added game class with state and state_hash

---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+require_all '../lib'
+class Game
+
+  attr_accessor :state
+  attr_reader :state_hash
+
+  def initialize
+    @state = 'start'
+    @state_hash = {'start' => ['y', 'n', 'list trainers'],
+                   'make trainer' => ['list pokemon', 'list moves'],
+                   'choose opponent' => ['list opponents', 'list pokemon'],
+                   'battle' => ['list moves', 'show hp', 'show opponent hp'],
+                   'post battle' => ['stats', 'play again']}
+  end
+
+end
