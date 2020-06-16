5408b24ede020ef68b0b1b8ffa2159a80fcfe889
DannyCoop <cooperdaniel5141@gmail.com>
2020-06-16 12:08:56 -0400

    started working on the  pick pokemon method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-#squirtle = Pokemon.create(name: "Squirtle", hp: 101)
-#charmander = Pokemon.create(name: "Charmander", hp: 99)
-#
-#danny_coop = Trainer.create(name: "Danny Cooper")
-#ian_mac = Trainer.create(name: "Ian MacKinnon")
-#
-#water_gun = Move.create(name: "Water Gun", damage: 30)
-#tackle = Move.create(name: "Tackle", damage: 25)
-#ember = Move.create(name: "Ember", damage: 35)
+squirtle = Pokemon.create(name: "Squirtle", hp: 101)
+charmander = Pokemon.create(name: "Charmander", hp: 99)
 
-#tp1 = TrainerPokemon.create(trainer_id: 2, pokemon_id: 1)
-#pm1 = PokemonMove.create(move_id: 1, trainer_pokemon_id: 1)
+danny_coop = Trainer.create(name: "Danny Cooper")
+ian_mac = Trainer.create(name: "Ian MacKinnon")
+
+water_gun = Move.create(name: "Water Gun", damage: 30)
+tackle = Move.create(name: "Tackle", damage: 25)
+ember = Move.create(name: "Ember", damage: 35)
+
+tp1 = TrainerPokemon.create(trainer_id: 2, pokemon_id: 1)
+pm1 = PokemonMove.create(move_id: 1, trainer_pokemon_id: 1)
 
--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,9 +1,16 @@
 class Trainer < ActiveRecord::Base
-
   has_many :trainer_pokemons
+  has_many :pokemons, through: :trainer_pokemons
 
   def pick_pokemon
-    
+    puts Pokemon.all
+    puts "Pick your first pokemon from the list above!!"
+    name = gets.chomp
+    if pokemon = Pokemon.find_by(name: name) 
+      TrainerPokemon.new(self.trainer_id, pokemon.pokemon_id)
+    elsif name = "help"
+      puts Game.help
+    else
+      puts "That is not one of the options"
   end
-
 end
